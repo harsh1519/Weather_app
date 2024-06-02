73401da35970441dc1f064b17f6dca2b4b04d641
Harsh Shah <harsh.shah.hs.2002@gmail.com>
2024-06-02 13:58:04 +0530

    Initial commit

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2d9b6dcb02f7d23d3f9edfaf97b11de9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
